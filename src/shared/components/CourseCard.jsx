001859d1ce1f5cc411c0181f196b9291bf6a8924
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-01-25 13:08:30 +0530

    Add star component in course card

--- src/shared/components/CourseCard.jsx
+++ src/shared/components/CourseCard.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import { course_1 } from '../../assets';
+import Stars from './Stars';
 
 const CourseCard = () => {
   return (
@@ -14,63 +15,7 @@ const CourseCard = () => {
       </a>
       <div className='p-2'>
         <div className='flex items-center space-x-3 mb-6'>
-          <div className='flex items-center space-x-1'>
-            <svg
-              className='w-5 h-5 text-yellow-500'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='currentColor'
-              viewBox='0 0 24 24'
-            >
-              <path d='M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z' />
-            </svg>
-            <svg
-              className='w-5 h-5 text-yellow-500'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='currentColor'
-              viewBox='0 0 24 24'
-            >
-              <path d='M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z' />
-            </svg>
-            <svg
-              className='w-5 h-5 text-yellow-500'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='currentColor'
-              viewBox='0 0 24 24'
-            >
-              <path d='M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z' />
-            </svg>
-            <svg
-              className='w-5 h-5 text-yellow-500'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='currentColor'
-              viewBox='0 0 24 24'
-            >
-              <path d='M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z' />
-            </svg>
-            <svg
-              className='w-5 h-5 text-yellow-disabled'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='currentColor'
-              viewBox='0 0 24 24'
-            >
-              <path d='M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z' />
-            </svg>
-          </div>
+          <Stars />
           <span className='bg-brand-softer border border-gray-300 text-gray-600 text-xs font-medium px-1.5 py-0.5 rounded-sm'>
             4.8 out of 5
           </span>
