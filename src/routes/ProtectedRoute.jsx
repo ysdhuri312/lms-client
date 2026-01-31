5be11de1da0549d87de03351cb449f9d04258a34
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-01-31 19:47:46 +0530

    Remove logs

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -5,7 +5,6 @@ import { useAuth } from '../features/auth/context/AuthContext';
 
 const ProtectedRoute = ({ role, children }) => {
   const { user } = useAuth();
-  console.log(user);
 
   if (!user) return <Navigate to='/login' />;
   return <>{children}</>;
