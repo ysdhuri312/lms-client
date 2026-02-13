7976d4e631fe1b1f9448b3171adbff90e5a64d0c
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-13 12:45:35 +0530

    Hook for fetch course and show error and loading state

---
+++ src/features/purchase/hooks/usePurchaseCourseDetails.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import { useEffect, useState } from 'react';
+import findCourseById from '../api/purchase.api';
+
+const usePurchaseCourseDetails = (slug) => {
+  const [purchaseCourse, setPurchaseCourse] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoding] = useState(true);
+
+  useEffect(() => {
+    findCourseById(slug)
+      .then((res) => setPurchaseCourse(res.data.course))
+      .catch((error) => setError(error))
+      .finally(() => setLoding(false));
+  }, []);
+
+  return { purchaseCourse, error, loading };
+};
+export default usePurchaseCourseDetails;
