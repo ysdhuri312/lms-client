39022530db447ab3f333f0c902cbb47e5f0b973f
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-17 19:58:55 +0530

    Fetch orders and verify payments through axios

--- src/features/purchase/api/purchase.api.js
+++ src/features/purchase/api/purchase.api.js
@@ -2,8 +2,33 @@
 
 import axios from '../../../shared/services/apiClient';
 
-const findCourseById = (slug) => {
+export const findCourseById = (slug) => {
   return axios.get(`/courses/${slug}`);
 };
 
-export default findCourseById;
+export const createOrder = async (orederDetails) => {
+  try {
+    const response = await axios.post('/orders', orederDetails);
+    return response.data;
+  } catch (error) {
+    const message =
+      error.response?.data?.message ||
+      error.message ||
+      'Ordered failed, Please order again';
+
+    throw new Error(message);
+  }
+};
+
+export const verifyPayment = async (orderDetails) => {
+  try {
+    const response = await axios.post('/orders/verify-payment', orderDetails);
+  } catch (error) {
+    const message =
+      error.response?.data?.message ||
+      error.message ||
+      'Ordered failed, Please order again';
+
+    throw new Error(message);
+  }
+};
