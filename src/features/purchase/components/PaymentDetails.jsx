19c72918fa892f809dc6254a11b8ba8dd19ef529
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-17 20:02:00 +0530

    Remove or commented unwanted code

--- src/features/purchase/components/PaymentDetails.jsx
+++ src/features/purchase/components/PaymentDetails.jsx
@@ -4,14 +4,14 @@ import { useState } from 'react';
 import usePurchaseCourseDetails from '../hooks/usePurchaseCourseDetails';
 
 const PaymentDetails = ({ slug }) => {
-  const { purchaseCourse, error, loading } = usePurchaseCourseDetails(slug);
+  // const { purchaseCourse, error, loading } = usePurchaseCourseDetails(slug);
 
-  const [selected, setSelected] = useState('card');
+  // const [selected, setSelected] = useState('card');
 
-  if (error) return <p>Somthing went worng...</p>;
-  if (loading) return <p>Courses loading...</p>;
+  // if (error) return <p>Somthing went worng...</p>;
+  // if (loading) return <p>Courses loading...</p>;
 
-  const { title, category, description, stats, modules } = purchaseCourse;
+  // const { title, category, description, stats, modules } = purchaseCourse;
 
   return (
     <div className='bg-white rounded-lg border p-6'>
@@ -34,78 +34,77 @@ const PaymentDetails = ({ slug }) => {
         purchases made in certain tax jurisdictions.
       </p>
 
-      <div className='flex justify-between items-center mb-3'>
+      {/* <div className='flex justify-between items-center mb-3'>
         <h3 className='font-medium'>Payment method</h3>
         <span className='text-xs text-gray-500'>🔒 Secure and encrypted</span>
       </div>
-
       <div>
-        {/* Option-1  */}
-        <div className='border rounded-md pt-4 pl-4 pr-4 mb-4'>
-          <div className='flex items-center gap-2 mb-4'>
-            <input
-              type='radio'
-              value='card'
-              name='payment-mode'
-              checked={selected === 'card'}
-              onChange={(e) => setSelected(e.target.value)}
-            />
-            <span className='text-sm font-medium'>Cards</span>
-          </div>
-
-          {selected === 'card' && (
-            <>
-              <input
-                type='text'
-                placeholder='Card number'
-                className='w-full border rounded-md px-3 py-2 text-sm mb-4'
-              />
-              <div className='grid grid-cols-2 gap-4 mb-4'>
-                <input
-                  type='text'
-                  placeholder='MM/YY'
-                  className='border rounded-md px-3 py-2 text-sm'
-                />
-                <input
-                  type='text'
-                  placeholder='CVC'
-                  className='border rounded-md px-3 py-2 text-sm'
-                />
-              </div>
-              <input
-                type='text'
-                placeholder='Name on card'
-                className='w-full border rounded-md px-3 py-2 text-sm mb-3'
-              />
-              <label className='flex items-center gap-2 text-sm text-gray-600 my-3'>
-                <input type='checkbox' />
-                Securely save this card for later purchase
-              </label>
-            </>
-          )}
+        
+      <div className='border rounded-md pt-4 pl-4 pr-4 mb-4'>
+        <div className='flex items-center gap-2 mb-4'>
+          <input
+            type='radio'
+            value='card'
+            name='payment-mode'
+            checked={selected === 'card'}
+            onChange={(e) => setSelected(e.target.value)}
+          />
+          <span className='text-sm font-medium'>Cards</span>
         </div>
 
-        {/* Option-2  */}
-        <div className='border rounded-md pt-4 pl-4 pr-4 mb-4'>
-          <div className='flex items-center gap-2 mb-4'>
-            <input
-              type='radio'
-              value='upi'
-              name='payment-mode'
-              checked={selected === 'upi'}
-              onChange={(e) => setSelected(e.target.value)}
-            />
-            <span className='text-sm font-medium'>UPI</span>
-          </div>
-          {selected === 'upi' && (
+        {selected === 'card' && (
+          <>
             <input
               type='text'
-              placeholder='UPI ID'
+              placeholder='Card number'
               className='w-full border rounded-md px-3 py-2 text-sm mb-4'
             />
-          )}
-        </div>
+            <div className='grid grid-cols-2 gap-4 mb-4'>
+              <input
+                type='text'
+                placeholder='MM/YY'
+                className='border rounded-md px-3 py-2 text-sm'
+              />
+              <input
+                type='text'
+                placeholder='CVC'
+                className='border rounded-md px-3 py-2 text-sm'
+              />
+            </div>
+            <input
+              type='text'
+              placeholder='Name on card'
+              className='w-full border rounded-md px-3 py-2 text-sm mb-3'
+            />
+            <label className='flex items-center gap-2 text-sm text-gray-600 my-3'>
+              <input type='checkbox' />
+              Securely save this card for later purchase
+            </label>
+          </>
+        )}
       </div>
+
+      
+      <div className='border rounded-md pt-4 pl-4 pr-4 mb-4'>
+        <div className='flex items-center gap-2 mb-4'>
+          <input
+            type='radio'
+            value='upi'
+            name='payment-mode'
+            checked={selected === 'upi'}
+            onChange={(e) => setSelected(e.target.value)}
+          />
+          <span className='text-sm font-medium'>UPI</span>
+        </div>
+        {selected === 'upi' && (
+          <input
+            type='text'
+            placeholder='UPI ID'
+            className='w-full border rounded-md px-3 py-2 text-sm mb-4'
+          />
+        )}
+      </div>
+    </div> */}
     </div>
   );
 };
