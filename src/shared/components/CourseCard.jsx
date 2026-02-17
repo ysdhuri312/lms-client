4d9c329d8080b9db72adc564c970cc8b79575774
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-17 20:14:36 +0530

    Add link on enroll button

--- src/shared/components/CourseCard.jsx
+++ src/shared/components/CourseCard.jsx
@@ -2,6 +2,7 @@
 
 import { Link } from 'react-router-dom';
 import Stars from './Stars';
+import { ShoppingBag } from 'lucide-react';
 
 const CourseCard = ({ slug, thumbnail, ratings, title, price }) => {
   return (
@@ -25,29 +26,14 @@ const CourseCard = ({ slug, thumbnail, ratings, title, price }) => {
         </Link>
         <div className='flex items-center justify-between mt-7'>
           <span className='text-xl font-bold'>${price}</span>
-          <button
+          <Link
+            to={`/checkout/${slug}`}
             type='button'
             className='inline-flex items-center cursor-pointer text-white bg-blue-600 hover:bg-blue-500 box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-sm text-xs px-2 py-1 focus:outline-none'
           >
-            <svg
-              className='w-4 h-4 me-1.5'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='none'
-              viewBox='0 0 24 24'
-            >
-              <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312'
-              />
-            </svg>
-            Add to cart
-          </button>
+            <ShoppingBag size={16} className='mr-2' />
+            Enroll Now
+          </Link>
         </div>
       </div>
     </div>
