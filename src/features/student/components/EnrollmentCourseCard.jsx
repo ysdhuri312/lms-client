b1df73067e476a34f1f2522f4f5c17158b818a04
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-23 20:13:38 +0530

    Feat: add enrolled courses dynamically which was purchase by user

--- src/features/student/components/EnrollmentCourseCard.jsx
+++ src/features/student/components/EnrollmentCourseCard.jsx
@@ -3,27 +3,30 @@
 import { Link } from 'react-router-dom';
 
 const EnrollmentCourseCard = ({ course }) => {
+  const { courseId, progress, status } = course;
   return (
     <tr className='border-t hover:bg-gray-100 transition-all'>
       <td className='px-6 py-4 cursor-pointer'>
         <div className='flex items-center gap-4 min-w-65'>
           <img
-            src={course.image}
-            alt={course.title}
+            src={courseId.thumbnail}
+            alt={courseId.title}
             className='w-24 h-14 object-cover rounded-xs'
           />
           <div className='flex-1'>
             <Link
-              to={`/course/${course.id}/lectures`}
+              to={`/course/${courseId._id}/lectures`}
               className='font-medium text-gray-900 mb-2'
             >
-              {course.title}
+              {courseId.title}
             </Link>
 
             <div className='w-full bg-gray-200 rounded-full h-1.5 mt-2'>
               <div
                 className='bg-blue-600 h-1.5 rounded-full'
-                style={{ width: `${course.progress}%` }}
+                style={{
+                  width: `${(progress.completedLessonCount / courseId.stats.totalLessons) * 100}%`,
+                }}
               />
             </div>
           </div>
@@ -31,16 +34,16 @@ const EnrollmentCourseCard = ({ course }) => {
       </td>
 
       <td className='px-6 py-4 text-gray-600 whitespace-nowrap'>
-        {course.duration}
+        {courseId.stats.courseTime}
       </td>
 
       <td className='px-6 py-4 text-gray-600 whitespace-nowrap'>
-        {course.completed}
+        {progress.completedLessonCount} / {courseId.stats.totalLessons} Lectures
       </td>
 
       <td className='px-6 py-4'>
         <span className='inline-block bg-blue-600 text-white text-xs px-3 py-2 rounded'>
-          {course.status}
+          {status}
         </span>
       </td>
     </tr>
--- src/features/student/pages/EnrollmentsPage.jsx
+++ src/features/student/pages/EnrollmentsPage.jsx
@@ -1,193 +1,13 @@
 /** @format */
 
 import EnrollmentCourseCard from '../components/EnrollmentCourseCard';
+import { useEnrolledCourses } from '../hooks/useEnrolledCourses';
 
 const EnrollmentsPage = () => {
-  const enrollments = [
-    {
-      id: 101,
-      title: 'Introduction to Cybersecurity',
-      image: 'https://picsum.photos/800/600',
-      duration: '1 hour, 1 minute',
-      completed: '1 / 4 Lectures',
-      progress: 25,
-      status: 'On Going',
-      modules: [
-        {
-          moduleId: 'm1',
-          title: 'Getting Started',
-          moduleTime: '49 hours, 30 minutes',
-          lessonsCount: 3,
-          lessons: [
-            {
-              lessonId: 'l1',
-              title: 'Welcome To The Course!',
-              lessonTime: '10 minutes',
-              preview: true,
-            },
-            {
-              lessonId: 'l2',
-              title: "ReactJS vs 'Vanilla JavaScript': Why Use React?",
-              lessonTime: '15 minutes',
-              preview: false,
-            },
-            {
-              lessonId: 'l3',
-              title: 'Editing Our First React App',
-              lessonTime: '7 minutes',
-              preview: false,
-            },
-          ],
-        },
-        {
-          moduleId: 'm2',
-          title: 'React Essentials - Components, JSX, Props, State & More',
-          lessons: [
-            {
-              lessonId: 'l1',
-              title: 'Module Introduction',
-              lessonTime: '3 minutes',
-              preview: true,
-            },
-            {
-              lessonId: 'l2',
-              title: 'JSX & React Components [Core Concept]',
-              lessonTime: '12 minutes',
-              preview: false,
-            },
-            {
-              lessonId: 'l3',
-              title: 'Using & Outputting Dynamic Values [Core Concept]',
-              lessonTime: '9 minutes',
-              preview: false,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 102,
-      title: 'Introduction to JavaScript',
-      image: 'https://picsum.photos/800/600',
-      duration: '1 hour, 5 minutes',
-      completed: '0 / 4 Lectures',
-      progress: 5,
-      status: 'On Going',
-      modules: [
-        {
-          moduleId: 'm1',
-          title: 'Getting Started',
-          moduleTime: '49 hours, 30 minutes',
-          lessonsCount: 3,
-          lessons: [
-            {
-              lessonId: 'l1',
-              title: 'Welcome To The Course!',
-              lessonTime: '10 minutes',
-              preview: true,
-            },
-            {
-              lessonId: 'l2',
-              title: "ReactJS vs 'Vanilla JavaScript': Why Use React?",
-              lessonTime: '15 minutes',
-              preview: false,
-            },
-            {
-              lessonId: 'l3',
-              title: 'Editing Our First React App',
-              lessonTime: '7 minutes',
-              preview: false,
-            },
-          ],
-        },
-        {
-          moduleId: 'm2',
-          title: 'React Essentials - Components, JSX, Props, State & More',
-          lessons: [
-            {
-              lessonId: 'l1',
-              title: 'Module Introduction',
-              lessonTime: '3 minutes',
-              preview: true,
-            },
-            {
-              lessonId: 'l2',
-              title: 'JSX & React Components [Core Concept]',
-              lessonTime: '12 minutes',
-              preview: false,
-            },
-            {
-              lessonId: 'l3',
-              title: 'Using & Outputting Dynamic Values [Core Concept]',
-              lessonTime: '9 minutes',
-              preview: false,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 103,
-      title: 'Introduction to JavaScript',
-      image: 'https://picsum.photos/800/600',
-      duration: '1 hour, 5 minutes',
-      completed: '0 / 4 Lectures',
-      progress: 5,
-      status: 'On Going',
-      modules: [
-        {
-          moduleId: 'm1',
-          title: 'Getting Started',
-          moduleTime: '49 hours, 30 minutes',
-          lessonsCount: 3,
-          lessons: [
-            {
-              lessonId: 'l1',
-              title: 'Welcome To The Course!',
-              lessonTime: '10 minutes',
-              preview: true,
-            },
-            {
-              lessonId: 'l2',
-              title: "ReactJS vs 'Vanilla JavaScript': Why Use React?",
-              lessonTime: '15 minutes',
-              preview: false,
-            },
-            {
-              lessonId: 'l3',
-              title: 'Editing Our First React App',
-              lessonTime: '7 minutes',
-              preview: false,
-            },
-          ],
-        },
-        {
-          moduleId: 'm2',
-          title: 'React Essentials - Components, JSX, Props, State & More',
-          lessons: [
-            {
-              lessonId: 'l1',
-              title: 'Module Introduction',
-              lessonTime: '3 minutes',
-              preview: true,
-            },
-            {
-              lessonId: 'l2',
-              title: 'JSX & React Components [Core Concept]',
-              lessonTime: '12 minutes',
-              preview: false,
-            },
-            {
-              lessonId: 'l3',
-              title: 'Using & Outputting Dynamic Values [Core Concept]',
-              lessonTime: '9 minutes',
-              preview: false,
-            },
-          ],
-        },
-      ],
-    },
-  ];
+  const { enrolledCourses, error, loading } = useEnrolledCourses();
+
+  if (error) return <p>Something went wrong...</p>;
+  if (loading) return <p>Courses loding...</p>;
 
   return (
     <section className='max-w-7xl mx-auto px-4 pt-30'>
@@ -205,8 +25,8 @@ const EnrollmentsPage = () => {
           </thead>
 
           <tbody>
-            {enrollments.map((course) => (
-              <EnrollmentCourseCard key={course.id} course={course} />
+            {enrolledCourses.map((course) => (
+              <EnrollmentCourseCard key={course._id} course={course} />
             ))}
           </tbody>
         </table>
